57c6b8ed3e24324d22e2e2e28d785858319d99cc
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-21 12:11:09 -0300

    feat: adiciona funcionalidade de scroll top

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../header/Header"
 import Footer from "../footer/Footer"
+import { useEffect } from "react"
 
 
 
 
 const Layout = () => {
+
+  const location = useLocation()
+
+  useEffect(() => {
+    
+    window.scrollTo(0, 0)
+  }, [location.pathname])
+
   return (
     
     <div>
