d90bda03408e63970c90216a1992a8afd5ef762b
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-21 11:33:23 -0300

    feat: adiciona rota com estilização inicial

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,16 @@
+
+
+import styles from "./Register.module.scss"
+
+
+const Register = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Opa, ainda estamos desenvolvendo essa parte</h1>
+      <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Fox.png" alt="Fox" width="25" height="25" />
+    </div>
+  )
+}
+
+export default Register
+
