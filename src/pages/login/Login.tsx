af65d740ded64344aaef6011ed29738c1fcfc910
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-21 11:33:13 -0300

    feat: adiciona rota com estilização inicial

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,13 @@
+import styles from "./Login.module.scss"
+
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Opa, ainda estamos desenvolvendo essa parte</h1>
+      <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Cat%20Face.png" alt="Cat Face" width="25" height="25" />
+    </div>
+  )
+}
+
+export default Login
