e5789b218706b63278fd3611c3caccce6c5977bd
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-21 23:28:05 -0300

    feat: adicione seo a rota home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,23 +7,32 @@ import styles from "./Home.module.scss"
 
 const Home = () => {
   return (
-    <main className={styles.home}>
-      <section id="inicio">
-        <Hero />
-      </section>
-      <section id="participar">
-        <Participate />
-      </section>
-      <section id="premios">
-        <Informations />
-      </section>
-      <section id="duvidas">
-        <Faq />
-      </section>
-      <section id="contato">
-        <Contact />
-      </section>
-    </main>
+    <>
+      <title>Promoção Doce Patinha – Concorra a prêmios incríveis para seu pet!</title>
+      <meta name="description" content="Participe da promoção Doce Patinha e concorra a prêmios incríveis para seu pet. Veja como participar, conheça os prêmios e tire suas dúvidas!" />
+      <meta name="keywords" content="promoção pet, doce patinha, sorteio pet, prêmios para pets, como participar, campanha animal, pets, cães, gatos, sorteio, cuidados com pets" />
+      <meta name="author" content="Doce Patinha" />
+      <meta name="robots" content="index, follow" />
+        
+      <main className={styles.home}>
+        <section id="inicio">
+          <Hero />
+        </section>
+        <section id="participar">
+          <Participate />
+        </section>
+        <section id="premios">
+          <Informations />
+        </section>
+        <section id="duvidas">
+          <Faq />
+        </section>
+        <section id="contato">
+          <Contact />
+        </section>
+      </main>
+    </>
+    
   )
 }
 
