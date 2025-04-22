265e95657880fbdf2df2b15b15fdf121e5fa4b28
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-21 23:27:32 -0300

    style: remove div e scroll

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,30 +1,16 @@
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import Header from "../header/Header"
 import Footer from "../footer/Footer"
-import { useEffect } from "react"
-
-
 
 
 const Layout = () => {
 
-  const location = useLocation()
-
-  useEffect(() => {
-    
-    window.scrollTo(0, 0)
-  }, [location.pathname])
-
   return (
-    
-    <div>
+    <>
       <Header />
-      <div>
-        <Outlet />
-      </div>
+      <Outlet />      
       <Footer />
-
-    </div>
+    </>
   )
 }
 
