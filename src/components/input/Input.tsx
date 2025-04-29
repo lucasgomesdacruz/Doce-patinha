99490f5e16ca0160e0e7639e933ef830875db0d6
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-29 16:31:08 -0300

    feat: adiciona novas props ao input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,25 +1,31 @@
 import styles from "./Input.module.scss"
+import { RegisterOptions, UseFormRegister } from "react-hook-form"
+
 
 interface InputProps {
-    type: string;
-    placeholder: string;
-    name: string;
-    label: string;
-  }
+  type: string;
+  placeholder: string;
+  name: string;
+  label: string;
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions;
+}
   
-  const Input = ({ type, placeholder, name, label }: InputProps) => {
-    return (
-      <div className={styles.containerInput}>
-        <label htmlFor={name}>{label}</label>
-        <input 
-          id={name}
-          type={type} 
-          placeholder={placeholder}
-          name={name}
-        />
-      </div>
-    )
-  }
+const Input = ({ type, placeholder, name, label, register, rules, error }: InputProps) => {
+  return (
+    <div className={styles.containerInput}>
+      <label htmlFor={name}>{label}</label>
+      <input 
+        id={name}
+        type={type} 
+        placeholder={placeholder}
+        {...register(name, rules)}
+      />
+      {error && <p>{error}</p>}
+    </div>
+  )
+}
   
   export default Input;
   
