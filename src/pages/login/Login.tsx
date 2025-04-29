9bbcd4a948f44b15443489c98502421ddb6b316a
Lucas Gomes <lucaass1997rp@gmail.com>
2025-04-28 22:14:35 -0300

    feat: adiciona estrutura e estilização de form entrar

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,17 +1,40 @@
 
 import styles from "./Login.module.scss"
 import useScrollToTop from "../../hooks/UseScrollToTop";
+import Input from "../../components/input/Input";
+import ButtonBrown from "../../components/buttonBrown/ButtonBrown";
+import { FaGoogle } from "react-icons/fa";
+import Button from "../../components/button/Button";
+import { RxEnter } from "react-icons/rx";
 
 
 const Login = () => {
   useScrollToTop();
   
-
   return (
-    <div className={styles.container}>
-      <h1>Opa, ainda estamos desenvolvendo essa parte</h1>
-      <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Cat%20Face.png" alt="Cat Face" width="25" height="25" />
-    </div>
+    <section className={styles.formBg}>
+      <div className={styles.formContainer}>
+        <h1>Entre com a sua conta</h1>
+        <ButtonBrown  icon={<FaGoogle />} text="Entrar com o Google"/>
+        <p>ou entre com o seu e-mail</p>
+        <form>
+          <Input
+            type="email"
+            placeholder="Digite seu e-mail"
+            name="email"
+            label="E-mail"
+          />
+          <Input
+            type="password"
+            placeholder="Digite sua senha"
+            name="password"
+            label="Senha"
+          />
+          <ButtonBrown  icon={<RxEnter />} text="Entrar"/>
+          <p>Primeiro acesso ? <span>Cadastrar</span></p>
+        </form>
+      </div>
+    </section>
   )
 }
 
