519fd134c398abe619542153a83a374dba4fb012
Lucas Gomes <lucaass1997rp@gmail.com>
2025-05-01 12:27:00 -0300

    feat: adiciona login via google

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,13 +4,15 @@ import useScrollToTop from "../../hooks/UseScrollToTop";
 import Input from "../../components/input/Input";
 import ButtonBrown from "../../components/buttonBrown/ButtonBrown";
 import { FaGoogle } from "react-icons/fa";
-// import Button from "../../components/button/Button";
 import { RxEnter } from "react-icons/rx";
 
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { auth } from "../../services/firebaseConnection";
 
 const schema = z.object({
   email: z.string().email("insira um email válido").min(1,"O campo é obrigatório."),
@@ -22,20 +24,41 @@ type FormData = z.infer<typeof schema>
 const Login = () => {
   useScrollToTop();
 
+  const navigate = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: "onChange"
   })
 
   function onSubmit(data:FormData) {
-    console.log(data)
+    signInWithEmailAndPassword(auth, data.email, data.password)
+    .then(() => {
+      console.log("longado com sucesso")
+      navigate("/", { replace: true })
+    })
+    .catch(err => {
+      console.log("errro ao longar")
+      console.log(err)
+    })
+  }
+
+  function handleGoogleLogin() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then(() => {
+        console.log("Login com Google bem-sucedido");
+        navigate("/", { replace: true });
+      })
+      .catch((error) => {
+        console.error("Erro ao logar com o Google:", error);
+      });
   }
   
   return (
     <section className={styles.formBg}>
       <div className={styles.formContainer}>
         <h1>Entre com a sua conta</h1>
-        <ButtonBrown  icon={<FaGoogle />} text="Entrar com o Google"/>
+        <ButtonBrown  icon={<FaGoogle />} text="Entrar com o Google" onClick={handleGoogleLogin}/>
         <p>ou entre com o seu e-mail</p>
 
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -55,7 +78,7 @@ const Login = () => {
             error={errors.password?.message}
             register={register}
           />
-          <ButtonBrown  icon={<RxEnter />} text="Entrar"/>
+          <ButtonBrown  icon={<RxEnter />} text="Entrar" type="submit" />
           <p>Primeiro acesso ? <Link to="/register">Cadastrar</Link></p>
         </form>
       </div>
